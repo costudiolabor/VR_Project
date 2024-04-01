479e179dc16791d9d6ea168de046ef0aeb390272
Solo <costudiolabor@gmail.com>
2024-04-01 12:48:57 +0300

    -

--- Assets/EnableDepthTextureMode.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class EnableDepthTextureMode : MonoBehaviour {
-    private void OnDrawGizmos() {
-        Camera.main.depthTextureMode = DepthTextureMode.Depth;
-        enabled = false;
-    }
-}
